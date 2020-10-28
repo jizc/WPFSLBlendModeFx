1d39c865a927896b8c12bcbd785cab849a96c862
Jan Ivar Z. Carlsen <jizc@outlook.com>
2020-10-28 13:22:46 +0100

    Migrated to Xaml Behaviors

--- WPFTestHarness/BackgroundEffectBehavior.cs
+++ WPFTestHarness/BackgroundEffectBehavior.cs
@@ -4,7 +4,7 @@ using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
 using System.Windows.Shapes;
-using System.Windows.Interactivity;
+using Microsoft.Xaml.Behaviors;
 
 namespace WPFTestHarness
 {
